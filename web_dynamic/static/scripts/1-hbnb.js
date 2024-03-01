051e880c78603edc288dcdd480abd1314c30b3f6
Cha1mae <chaimastaifa@gmail.com>
2024-03-01 11:16:37 -0800

    checkingg

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,18 +1,19 @@
-$(document).ready(function() {
-    var amenities = {};
+/*checking if its correct*/
+$(document).ready(() => {
+  const amenities = {};
 
-    $('input[type="checkbox"]').change(function() {
-        var id = $(this).data('id');
-        var name = $(this).data('name');
+  $('input[type="checkbox"]').change(function () {
+    const id = $(this).data('id');
+    const name = $(this).data('name');
 
-        if ($(this).is(":checked")) {
-            amenities[id] = name;
-        } else {
-            delete amenities[id];
-        }
+    if ($(this).is(':checked')) {
+      amenities[id] = name;
+    } else {
+      delete amenities[id];
+    }
 
-        var amenitiesList = Object.values(amenities).join(', ') || "No amenities selected";
-        $('.amenities h4').text("Amenities: ");
-        $('.amenities h4').next('p').text(amenitiesList);
-    });
+    const amenitiesList = Object.values(amenities).join(', ') || 'No amenities selected';
+    $('.amenities h4').text('Amenities: ');
+    $('.amenities h4').next('p').text(amenitiesList);
+  });
 });
